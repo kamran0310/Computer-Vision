abd01c73d768a9f03d7f65899ff51590b1d3952f
kamran0310 <kamranaqeeltechie@gmail.com>
2025-03-14 23:19:45 +0500

    added code for saving and showing image

--- Module-5/Flask App/app/views.py
+++ Module-5/Flask App/app/views.py
@@ -15,9 +15,34 @@ def genderapp():
     
     if request.method=='POST':
         f=request.files['image_name']
-        filename=f.filename
+        filename=f.filename 
         path=os.path.join(UPLOAD_FOLDER, filename)
         f.save(path)
+        pred_image, predictions = faceRecognitionPipeline(path)
+        pred_filename = 'prediction_image.jpg'
+        cv2.imwrite(f"./static/predict/{pred_filename}", pred_image)
+        print(predictions)
+        
+        report = []
+        
+        for i, obj in enumerate(predictions):
+            gray_img = obj['roi']
+            eign_img = obj['eig_img'].reshape(100,100)
+            gender_name = obj['prediction_name']
+            score = round(obj['score']*100,2)
+            
+            gray_img_name = f'roi_{i}.jpg'
+            eig_img_name = f'eigen_{i}.jpg'
+            
+            matimg.imsave(f'./static/predict/{gray_img_name}', gray_img, cmap='gray')
+            matimg.imsave(f'./static/predict/{eig_img_name}', eign_img, cmap='gray')
+            
+            report.append([gray_img_name, eig_img_name, gender_name, score])
+        return render_template('gender.html', fileupload=True, report=report)
+    print("Machine Learning Model executed successfully")
+            
+        
+        
     
     
     return render_template('gender.html')
