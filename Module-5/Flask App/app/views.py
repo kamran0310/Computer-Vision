c254d769206ebd9101ff20e8610320892c38e81d
kamran0310 <kamranaqeeltechie@gmail.com>
2025-03-14 22:47:28 +0500

    added code for bootstrap

--- Module-5/Flask App/app/views.py
+++ Module-5/Flask App/app/views.py
@@ -1,7 +1,9 @@
 from flask import render_template, request
+import os
+import cv2
+from app.face_recognition import faceRecognitionPipeline
 
-
-
+UPLOAD_FOLDER='static/upload'
 def index():
     return render_template('index.html')
 
@@ -10,4 +12,12 @@ def app():
     return render_template('app.html')
 
 def genderapp():
+    
+    if request.method=='POST':
+        f=request.files['image_name']
+        filename=f.filename
+        path=os.path.join(UPLOAD_FOLDER, filename)
+        f.save(path)
+    
+    
     return render_template('gender.html')
